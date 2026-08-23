a904dbceeef86a33344708ef7a27ed066d59b06b
Phillip Jernigan <devopsprofed@gmail.com>
2026-08-23 15:02:25 -0500

    Deploy to test: 2026-08-23 15:02

---
+++ _temp_query.py
@@ -0,0 +1,8 @@
+﻿import os, django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+from cases.models import Case, UnreadMessage
+for c in Case.objects.filter(status__in=['cancelled','declined'])[:10]:
+    ums = list(UnreadMessage.objects.filter(case=c).values_list('user__username','user__role'))
+    if ums:
+        print(c.external_case_id, c.status, ums)
